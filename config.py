8f37a59e4d80e6fcac17a09716ab1a6c2a304770
Sunatte <118675750+sunatte-saad@users.noreply.github.com>
2025-03-09 00:32:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-MODEL_PATH = r"C:\Users\Saad\Desktop\docai_ret\vision_model\moondream-0_5b-int8.mf" # or replace by C:\Users\Saad\Desktop\docai_ret\vision_model\moondream-2b-int8.mf
+MODEL_PATH = r"vision_model\moondream-0_5b-int8.mf" # or replace by "vision_model\moondream-2b-int8.mf"
 FRAME_INTERVAL = 120
 MAX_FRAMES = 5
 RESIZE_DIM = (640, 360)
